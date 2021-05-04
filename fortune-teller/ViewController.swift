5e3ceac2a0d97172030b518c5c6565ece0c94839
Jhen <79475798+jhen0605@users.noreply.github.com>
2021-05-05 04:18:45 +0800

    Initial Commit

---
+++ fortune-teller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fortune-teller
+//
+//  Created by 簡吟真 on 2021/5/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
